f0aea1735db378139bf6cb4a65b2779cc744d153
Maya Rao <rao.maya@gmail.com>
2012-10-21 14:10:48 -0400

    add pirate

---
+++ fantastic/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fantastic::Application.config.secret_token = '006d7de2b6e49f0355c6a0f9394007ba3b3de9b8683ffc90a5b7d9543049934f0074eb9ffed3e9260a6b8a13211a526f4b1ed46b07fcfb0ef9d33568c20e8e41'
